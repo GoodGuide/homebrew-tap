7d89e33b26bb3f4d85088c0f9f86751a0f7376b6
Ryan Taylor Long <ryan@rtlong.com>
2014-11-18 16:32:52 -0800

    Update GoodguideGitHooks

--- goodguide-git-hooks.rb
+++ goodguide-git-hooks.rb
@@ -1,8 +1,8 @@
 class GoodguideGitHooks < Formula
   homepage "https://github.com/GoodGuide/goodguide-git-hooks"
 
-  version "0.0.4"
-  sha1 "5a47f683168432328dd39bc939096de12f35d8b3"
+  version "0.0.5"
+  sha1 "61e4355fc7c5a9d816b6aaaef315b50a1b14cc8f"
 
   url "https://github.com/GoodGuide/goodguide-git-hooks/releases/download/v#{version}/goodguide-git-hooks_#{version}_darwin_amd64.tar.gz"
 
