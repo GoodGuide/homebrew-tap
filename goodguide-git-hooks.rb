a238aaa6e740fa25e9a59fb502d718a28c69501a
Ryan Taylor Long <ryan@rtlong.com>
2016-09-20 16:02:53 +0100

    Update version of goodguide-git-hooks
    [no story]

--- goodguide-git-hooks.rb
+++ goodguide-git-hooks.rb
@@ -1,8 +1,8 @@
 class GoodguideGitHooks < Formula
   homepage 'https://github.com/GoodGuide/goodguide-git-hooks'
 
-  version '0.0.8'
-  sha256 'd540883bf77f57ea8984917d9e0237a88db48402fee62ab29ba637895d1d1d7a'
+  version '0.0.9'
+  sha256 '45370c569cd366caed9883403f63eac82cb7c285cae0acecb3542e4d1b598d11'
 
   url "https://github.com/GoodGuide/goodguide-git-hooks/releases/download/v#{version}/goodguide-git-hooks_#{version}_darwin_amd64.tar.gz"
 
