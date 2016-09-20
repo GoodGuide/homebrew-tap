d028e67d5b26caf78cfa0cea3fc1a7cb8d5a2932
Ryan Taylor Long <ryan@rtlong.com>
2016-09-20 02:33:44 +0100

    Add sha256 checksums for newer homebrew
    [no story]

--- goodguide-git-hooks.rb
+++ goodguide-git-hooks.rb
@@ -1,8 +1,8 @@
 class GoodguideGitHooks < Formula
-  homepage "https://github.com/GoodGuide/goodguide-git-hooks"
+  homepage 'https://github.com/GoodGuide/goodguide-git-hooks'
 
-  version "0.0.8"
-  sha1 "8202fae29f6349096f7b691ac3dc48fa7c9472df"
+  version '0.0.8'
+  sha256 'd540883bf77f57ea8984917d9e0237a88db48402fee62ab29ba637895d1d1d7a'
 
   url "https://github.com/GoodGuide/goodguide-git-hooks/releases/download/v#{version}/goodguide-git-hooks_#{version}_darwin_amd64.tar.gz"
 
--- osx-remap-keyboard-modifiers.rb
+++ osx-remap-keyboard-modifiers.rb
@@ -1,8 +1,8 @@
 class OsxRemapKeyboardModifiers < Formula
-  homepage "https://github.com/rtlong/osx-remap-keyboard-modifiers"
+  homepage 'https://github.com/rtlong/osx-remap-keyboard-modifiers'
 
-  version "0.0.1"
-  sha1 "35a893c84cfe69fc55944f391cc9e88d9452eb09"
+  version '0.0.1'
+  sha256 'a7dac75ae87f2c073ba92e02616d4c37e4bcf75974debdc9f5b28a09815afe9a'
 
   url "https://github.com/rtlong/osx-remap-keyboard-modifiers/releases/download/v#{version}/osx-remap-keyboard-modifiers_#{version}_darwin_amd64.tar.gz"
 
